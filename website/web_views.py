8341a3fe1dd11a88a5d92ee2b67b3223d007d085
Phil White <philipcwhite@users.noreply.github.com>
2018-12-19 09:49:27 -0600

    2018-12-19

--- website/web_views.py
+++ website/web_views.py
@@ -202,3 +202,14 @@ class WebViews:
         </table>
         </form>"""
         return html
+    
+    def load_user_edit_password():
+        html = """
+        <form action="" method="POST">
+        <table>
+        <tr><td style="width:150px">Old Password</td><td style="width:150px"><input type="password" class="text-input" name="pass1" /></td></tr>
+        <tr><td>New Password</td><td><input type="password" class="text-input" name="pass2" /></td></tr>
+        <tr><td></td><td style="text-align:right"><input type="submit" class="action-button" value="Submit" /></td></tr>
+        </table>
+        </form>"""
+        return html
