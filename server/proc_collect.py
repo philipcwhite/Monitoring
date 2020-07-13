496a8540e8c755f9b70173412ca1a132cbdca4f1
Phil White <philipcwhite@users.noreply.github.com>
2020-07-13 16:26:54 -0400

    Update proc_collect.py

--- server/proc_collect.py
+++ server/proc_collect.py
@@ -36,63 +36,48 @@ class CollectLoad:
         except: pass
       
 class CollectData:
-    def mon_con():
-        connection = pymysql.connect(host = CollectSettings.dbhost,
-                                     user = CollectSettings.dbuser,
-                                     password = CollectSettings.dbpassword,
-                                     db = CollectSettings.database,
-                                     charset = 'utf8mb4',
-                                     cursorclass = pymysql.cursors.DictCursor)
-        return connection
+    def __init__(self):
+        self.con = pymysql.connect(host = CollectSettings.dbhost, user = CollectSettings.dbuser, password = CollectSettings.dbpassword,
+                                   db = CollectSettings.database, charset = 'utf8mb4', cursorclass = pymysql.cursors.DictCursor)
+        self.cursor = self.con.cursor()
+    
+    def __del__(self):
+        self.con.close()
 
-    def agent_system(timestamp, name, domain, ipaddress, platform, build, architecture, processors, memory):
-        connection = CollectData.mon_con()
-        try:
-            with connection.cursor() as cursor:
-                sql = "SELECT name FROM agentsystem where name=%s"
-                cursor.execute(sql, (name))
-                result = cursor.fetchone()
-                qname = ""            
-                if not result is None:
-                    qname = result['name']
-                if name == qname:
-                    sql = "UPDATE agentsystem SET timestamp=%s, domain=%s, ipaddress=%s, platform=%s, build=%s, architecture=%s, processors=%s, memory=%s WHERE name=%s"
-                    cursor.execute(sql, (timestamp, domain, ipaddress, platform, build, architecture, processors, memory, name))
-                    connection.commit()
-                else:
-                    sql = "INSERT INTO agentsystem (timestamp, name, domain, ipaddress, platform, build, architecture, processors, memory) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-                    cursor.execute(sql, (timestamp, name, domain, ipaddress, platform, build, architecture, processors, memory))
-                    connection.commit()
-        finally: connection.close()
+    def agent_system(self, timestamp, name, domain, ipaddress, platform, build, architecture, processors, memory):
+        sql = "SELECT name FROM agentsystem where name=%s"
+        self.cursor.execute(sql, (name))
+        result = self.cursor.fetchone()
+        qname = ""            
+        if not result is None:
+            qname = result['name']
+            if name == qname:
+                sql = "UPDATE agentsystem SET timestamp=%s, domain=%s, ipaddress=%s, platform=%s, build=%s, architecture=%s, processors=%s, memory=%s WHERE name=%s"
+                self.cursor.execute(sql, (timestamp, domain, ipaddress, platform, build, architecture, processors, memory, name))
+                self.con.commit()
+            else:
+                sql = "INSERT INTO agentsystem (timestamp, name, domain, ipaddress, platform, build, architecture, processors, memory) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+                self.cursor.execute(sql, (timestamp, name, domain, ipaddress, platform, build, architecture, processors, memory))
+                self.con.commit()
 
-    def agent_data(timestamp, name, monitor, value):
-        connection = CollectData.mon_con()
-        try:
-            with connection.cursor() as cursor:
-                sql = "INSERT INTO agentdata (timestamp, name, monitor, value) VALUES(%s,%s,%s,%s)"
-                cursor.execute(sql, (timestamp, name, monitor, value))
-                connection.commit() 
-        finally: connection.close()
+    def agent_data(self, timestamp, name, monitor, value):
+        sql = "INSERT INTO agentdata (timestamp, name, monitor, value) VALUES(%s,%s,%s,%s)"
+        self.cursor.execute(sql, (timestamp, name, monitor, value))
+        self.con.commit() 
 
-    def agent_events_open(timestamp, name, monitor, message, status, severity):
-        connection = CollectData.mon_con()
-        try:
-            with connection.cursor() as cursor:
-                sql = "INSERT INTO agentevents (timestamp, name, monitor, message, status, severity, processed) VALUES(%s,%s,%s,%s,%s,%s,%s)"
-                cursor.execute(sql, (timestamp, name, monitor, message, status, severity, 0))
-                connection.commit()
-        finally: connection.close()
+    def agent_events_open(self, timestamp, name, monitor, message, status, severity):
+        sql = "INSERT INTO agentevents (timestamp, name, monitor, message, status, severity, processed) VALUES(%s,%s,%s,%s,%s,%s,%s)"
+        self.cursor.execute(sql, (timestamp, name, monitor, message, status, severity, 0))
+        self.con.commit()
 
-    def agent_events_close(name, monitor, severity):
-        connection = CollectData.mon_con()
-        try:
-            with connection.cursor() as cursor:
-                sql = "UPDATE agentevents SET status=0 AND processed=0 WHERE name=%s AND monitor=%s AND severity=%s AND status=1"
-                cursor.execute(sql, (name, monitor, severity))
-                connection.commit()
-        finally: connection.close()
+    def agent_events_close(self, name, monitor, severity):
+        sql = "UPDATE agentevents SET status=0 AND processed=0 WHERE name=%s AND monitor=%s AND severity=%s AND status=1"
+        self.cursor.execute(sql, (name, monitor, severity))
+        self.con.commit()
 
+class CollectParse:
     def parse_data(message):
+        CD = CollectData()
         try:
             message = json.loads(message)
             passphrase = message["passphrase"]
@@ -106,9 +91,9 @@ class CollectData:
                 architecture = message["arch"] 
                 processors = message["procs"]
                 memory = message["memory"] 
-                CollectData.agent_system(timestamp, name, domain, ipaddress, platform, build, architecture, int(processors), float(memory))
+                CD.agent_system(timestamp, name, domain, ipaddress, platform, build, architecture, int(processors), float(memory))
                 for i in message["data"]:
-                    CollectData.agent_data(i[list(i)[0]], name, list(i)[1], i[list(i)[1]])
+                    CD.agent_data(i[list(i)[0]], name, list(i)[1], i[list(i)[1]])
                 for i in message["events"]:
                     timestamp = i["time"]
                     name = name
@@ -117,9 +102,9 @@ class CollectData:
                     status = i["status"]
                     severity = i["severity"]
                     if int(status) == 1:
-                        CollectData.agent_events_open(timestamp, name, monitor, message, status, severity)
+                        CD.agent_events_open(timestamp, name, monitor, message, status, severity)
                     elif int(status) == 0:
-                        CollectData.agent_events_close(name, monitor, severity)
+                        CD.agent_events_close(name, monitor, severity)
         except: pass
 
 class EchoServerProtocol(asyncio.Protocol):
@@ -131,7 +116,7 @@ class EchoServerProtocol(asyncio.Protocol):
             loop = asyncio.get_running_loop()
             loop.call_soon_threadsafe(loop.stop)
         message = data.decode()
-        CollectData.parse_data(message)
+        CollectParse.parse_data(message)
         self.transport.write(b'Received')
         self.transport.close()
 
