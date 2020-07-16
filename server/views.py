9e18376fcd15ce5328e29a9022a07acd7afbab73
Phil White <philipcwhite@users.noreply.github.com>
2020-07-16 16:30:40 -0400

    Update views.py

--- server/views.py
+++ server/views.py
@@ -34,7 +34,7 @@ class controller(object):
 
     def events(self, status=1):
         user = self.get_auth()
-        html = WebViews.load_base(user, WebViews.load_bc_events(), WebViews.load_refresh('/events_content/' + str(status)))
+        html = render('base.html', user = user, breadcrumbs = WebViews.load_bc_events(), body = WebViews.load_refresh('/events_content/' + str(status)))
         return html
 
     def events_content(self, status=1):
@@ -50,11 +50,11 @@ class controller(object):
     def devices(self, name=None, monitor=None):
         user = self.get_auth()
         if name is None and monitor is None:
-            html = WebViews.load_base(user, WebViews.load_bc_devices(), WebViews.load_basic_page('Devices', WebDevices.device_index()))
+            html = render('base.html', user = user, breadcrumbs = WebViews.load_bc_devices(), body = WebViews.load_basic_page('Devices', WebDevices.device_index()))
         elif not name is None and monitor is None:
-            html = WebViews.load_base(user, WebViews.load_bc_device(name), WebViews.load_refresh('/device_content/' + name))
+            html = render('base.html', user = user, breadcrumbs = WebViews.load_bc_device(name), body = WebViews.load_refresh('/device_content/' + name))
         elif not name is None and not monitor is None:
-            html = WebViews.load_base(user, WebViews.load_bc_device_graph(name, monitor), WebViews.load_refresh('/device_graph_content/' + name + '/' + monitor))
+            html = render('base.html', user = user, breadcrumbs = WebViews.load_bc_device_graph(name, monitor), body = WebViews.load_refresh('/device_graph_content/' + name + '/' + monitor))
         return html
     
     def device_content(self, name):
@@ -69,7 +69,7 @@ class controller(object):
     
     def reports(self):
         user = self.get_auth()
-        html = WebViews.load_base(user, WebViews.load_bc_reports(), WebViews.load_basic_page('Reports', WebViews.load_reports()))
+        html = render('base.html', user = user, breadcrumbs = WebViews.load_bc_reports(), body = WebViews.load_basic_page('Reports', WebViews.load_reports()))
         return html
     
     def report(self, filename):
@@ -84,24 +84,24 @@ class controller(object):
 
     def settings(self):
         user = self.get_auth()
-        html = WebViews.load_base(user, WebViews.load_bc_settings(),  WebViews.load_basic_page('Settings', WebSettings.settings()))
+        html = render('base.html', user = user, breadcrumbs = WebViews.load_bc_settings(),  body = WebViews.load_basic_page('Settings', WebSettings.settings()))
         return html
 
     def about(self):
         user = self.get_auth()
-        html = WebViews.load_base(user, WebViews.load_bc_settings(),  WebViews.load_basic_page('About', WebSettings.about()))
+        html = render('base.html', user = user, breadcrumbs = WebViews.load_bc_settings(), body = WebViews.load_basic_page('About', WebSettings.about()))
         return html
 
     def notify(self):
         user = self.get_auth()
-        html = WebViews.load_base(user, WebViews.load_bc_settings(), WebViews.load_basic_page('Notification Rules', WebNotify.notify_rules()))
+        html = render('base.html', user = user, breadcrumbs = WebViews.load_bc_settings(), body = WebViews.load_basic_page('Notification Rules', WebNotify.notify_rules()))
         return html
     
     def notify_add(self, notify_name = None, notify_email = None, agent_name = None, agent_monitor = None, agent_status = None, agent_severity = None, notify_enabled = None):
         user = self.get_auth()
         html=''
         if notify_name is None and notify_email is None and agent_name is None and agent_monitor is None and agent_status is None and agent_severity is None and notify_enabled is None:
-            html = WebViews.load_base(user, WebViews.load_bc_settings(), WebViews.load_basic_page('Add Notification Rule', WebNotify.notify_add()))
+            html = render('base.html', user = user, breadcrumbs = WebViews.load_bc_settings(), body = WebViews.load_basic_page('Add Notification Rule', WebNotify.notify_add()))
             return html
         else:
             WD.web_code_insert_notifyrules(notify_name, notify_email, agent_name, agent_monitor, agent_status, agent_severity, notify_enabled)
@@ -111,7 +111,7 @@ class controller(object):
         user = self.get_auth()
         html=''
         if notify_name is None and notify_email is None and agent_name is None and agent_monitor is None and agent_status is None and agent_severity is None and notify_enabled is None:
-            html = WebViews.load_base(user, WebViews.load_bc_settings(), WebViews.load_basic_page('Edit Notification Rule', WebNotify.notify_edit(id)))
+            html = render('base.html', user = user, breadcrumbs = WebViews.load_bc_settings(), body = WebViews.load_basic_page('Edit Notification Rule', WebNotify.notify_edit(id)))
             return html
         else:
             WD.web_code_update_notifyrules(id, notify_name, notify_email, agent_name, agent_monitor, agent_status, agent_severity, notify_enabled)
@@ -124,7 +124,7 @@ class controller(object):
 
     def users(self):
         user = self.get_auth()
-        html = WebViews.load_base(user, WebViews.load_bc_settings(), WebViews.load_basic_page('Users', WebUsers.user_list()))
+        html = render('base.html', user = user, breadcrumbs = WebViews.load_bc_settings(), body = WebViews.load_basic_page('Users', WebUsers.user_list()))
         return html
 
     def user_add(self, username=None, password=None, role=None):
@@ -133,13 +133,13 @@ class controller(object):
             WebUsers.user_add(username, password, role)
             self.redirect('/users')
         else:
-            html = WebViews.load_base(user, WebViews.load_bc_settings(), WebViews.load_basic_page('Users', WebViews.load_user_add()))
+            html = render('base.html', user = user, breadcrumbs = WebViews.load_bc_settings(), body = WebViews.load_basic_page('Users', WebViews.load_user_add()))
             return html
 
     def user_edit_pass(self, id, pass1=None, pass2=None):
         user = self.get_auth()
         if pass1 is None and pass2 is None:
-            html = WebViews.load_base(user, WebViews.load_bc_settings(), WebViews.load_basic_page('Users', WebViews.load_user_edit_password()))
+            html = render('base.html', user = user, breadcrumbs = WebViews.load_bc_settings(), body = WebViews.load_basic_page('Users', WebViews.load_user_edit_password()))
             return html
         else:
             encryptpw = WebAuth.set_password(pass1, pass2)
@@ -152,7 +152,7 @@ class controller(object):
         user = self.get_auth()
         if role == None: 
             roleid = WD.web_code_select_user(id)
-            html = WebViews.load_base(user, WebViews.load_bc_settings(), WebViews.load_basic_page('Users', WebViews.load_user_edit_role(roleid['role'])))
+            html = render('base.html', user = user, breadcrumbs = WebViews.load_bc_settings(), body = WebViews.load_basic_page('Users', WebViews.load_user_edit_role(roleid['role'])))
             return html
         else:
             WD.web_code_edit_user_role(id, role)
@@ -161,7 +161,7 @@ class controller(object):
     def user_delete(self, id):
         user = self.get_auth()
         #Add confirmation Page
-        html = WebViews.load_base(user, WebViews.load_bc_settings(), WebViews.load_basic_page('Users', WebViews.load_confirm_delete(id)))
+        html = render('base.html', user = user, breadcrumbs = WebViews.load_bc_settings(), body = WebViews.load_basic_page('Users', WebViews.load_confirm_delete(id)))
         return html
 
     def user_delete_confirm(self, id):
@@ -170,18 +170,18 @@ class controller(object):
 
     def help(self):
         user = self.get_auth()
-        html = WebViews.load_base(user, WebViews.load_bc_settings(),  WebViews.load_basic_page('Help', render('help.html')))
+        html = render('base.html', user = user, breadcrumbs =  WebViews.load_bc_settings(), body = WebViews.load_basic_page('Help', render('help.html')))
         return html
           
     def search(self, device=None):
         user = self.get_auth()
-        html = WebViews.load_base(user, WebViews.load_bc_settings(), WebViews.load_basic_page('Search Results', WebSearch.search_devices(device)))
+        html = render('base.html', user = user, breadcrumbs = WebViews.load_bc_settings(), body = WebViews.load_basic_page('Search Results', WebSearch.search_devices(device)))
         return html
 
     def password(self, pass1=None, pass2=None):
         user = self.get_auth()
         if pass1 is None and pass2 is None:
-            html = WebViews.load_base(user, WebViews.load_bc_settings(), WebViews.load_basic_page('Change Password', WebViews.load_change_password()))
+            html = render('base.html', user = user, breadcrumbs = WebViews.load_bc_settings(), body = WebViews.load_basic_page('Change Password', WebViews.load_change_password()))
             return html
         else:
             changepw = WebAuth.change_password(user, pass1, pass2)
@@ -190,32 +190,11 @@ class controller(object):
 
     def error(self):
         user = self.get_auth()
-        html = WebViews.load_base(user, WebViews.load_bc_settings(),  WebViews.load_basic_page('Error', 'Error'))
+        html = render('base.html', user = user, breadcrumbs = WebViews.load_bc_settings(),  body = WebViews.load_basic_page('Error', 'Error'))
         return html
 
-'''def load_config():
-    try:
-        parser = configparser.ConfigParser()
-        parser.read(app_vars.app_path + 'settings.ini')
-        certificates = dict(parser.items('certificates'))
-        database = dict(parser.items('database'))
-        server = dict(parser.items('server'))
-        app_vars.server_port = int(server['port_web'])
-        app_vars.session_expire = int(server['session_expire'])
-        app_vars.ssl_enabled = eval(server['secure'])
-        app_vars.cert_key = certificates['key']
-        app_vars.cert_name = certificates['name']
-        DataConf.host = database['host']
-        DataConf.db = database['name']
-        DataConf.user = database['user']
-        DataConf.password = database['password']
-        #print(database['host'])
-        print(DataConf.host)
-        print(DataConf.password)
-    except: pass'''
-
-
 def load_config():
+    # Add Try block
     parser = configparser.ConfigParser()
     parser.read('settings.ini')
     #parser.read(app_vars.app_path + 'settings.ini')
