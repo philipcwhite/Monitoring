3c8111635168d89ea8493a123a6ae6faf5354b5f
Phil White <philipcwhite@users.noreply.github.com>
2020-11-25 15:11:45 -0500

    Add files via upload
    Flask site

---
+++ server/install/server/app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import os
+
+# Initialize Flask
+app = Flask(__name__, instance_relative_config=True)
+
+# Add secret key for authentication
+app.secret_key = os.urandom(12)
+
+# Import views
+from monitoring import views
+
+# Load Flask Configuration (instance/flask.cfg)
+app.config.from_pyfile('flask.cfg')
+
+# Initialize database
+from .model import Auth, Data
+D = Data()
+A = Auth()
+A.user_initialize() 
---
+++ server/install/server/run.py
@@ -0,0 +1,5 @@
+from home import app
+
+if __name__ == '__main__':
+    #app.run()
+    app.run()
